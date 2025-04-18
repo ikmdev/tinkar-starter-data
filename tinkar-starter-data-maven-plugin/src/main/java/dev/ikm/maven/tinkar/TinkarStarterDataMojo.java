9a7e47b579563042c6ec506a944a09d7a3f3f532
Aras <caras@deloitte.com>
2025-04-18 09:43:24 -0400

    new grouping changes

--- tinkar-starter-data-maven-plugin/src/main/java/dev/ikm/maven/tinkar/TinkarStarterDataMojo.java
+++ tinkar-starter-data-maven-plugin/src/main/java/dev/ikm/maven/tinkar/TinkarStarterDataMojo.java
@@ -2795,12 +2795,12 @@ public class TinkarStarterDataMojo extends SimpleTinkarMojo {
 
         session.compose((ConceptAssembler conceptAssembler) -> conceptAssembler.concept(GROUPING))
                 .attach((FullyQualifiedName fqn) -> fqn
-                        .text("Health concept (SOLOR)")
+                        .text("Grouping (SOLOR)")
                         .language(ENGLISH_LANGUAGE)
                         .caseSignificance(DESCRIPTION_NOT_CASE_SENSITIVE)
                         .attach(usDialect()))
                 .attach((Synonym synonym) -> synonym
-                        .text("Health concept")
+                        .text("Grouping")
                         .caseSignificance(DESCRIPTION_NOT_CASE_SENSITIVE)
                         .language(ENGLISH_LANGUAGE)
                         .attach(usDialect()))
